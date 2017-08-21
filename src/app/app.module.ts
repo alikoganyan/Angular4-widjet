2c92515ee79f026e356796519f0ac5be4c0680b7
alikoganyan <alik.oganyan@gmail.com>
2017-08-21 16:39:27 +0400

    create salonListComponent, add   UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TownSelectionComponent } from './town-selection/town-selection.component';
+import { SalonListComponent } from './salon-list/salon-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TownSelectionComponent
+    TownSelectionComponent,
+    SalonListComponent
   ],
   imports: [
     BrowserModule
