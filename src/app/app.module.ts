075e8076e4e3ae56c871257ce62efceed01cc402
alikoganyan <alik.oganyan@gmail.com>
2017-08-21 17:14:54 +0400

    create selectSpecialist component, add UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TownSelectionComponent } from './town-selection/town-selection.component';
 import { SalonListComponent } from './salon-list/salon-list.component';
+import { SelectSpecialistComponent } from './select-specialist/select-specialist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TownSelectionComponent,
-    SalonListComponent
+    SalonListComponent,
+    SelectSpecialistComponent
   ],
   imports: [
     BrowserModule
